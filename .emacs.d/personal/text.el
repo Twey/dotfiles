0b5f121c884f774f5f0e4f41d96b3166a8886c86
James Kay <james@hadean.com>
2019-02-13 19:11:33 +0000

    move settings into customization

--- .emacs.d/personal/text.el
+++ .emacs.d/personal/text.el
@@ -1,8 +1,9 @@
 (require 'org-bullets)
+(defface org-bullets
+  '((t :family "DejaVu Sans Mono"))
+  "Face used for highlighting bullets in org-bullets-mode.")
 (add-hook
  'text-mode-hook
  (lambda ()
    (variable-pitch-mode 1)
-   (blink-cursor-mode 0)    ;; Reduce visual noise
-   (setq org-bullets-bullet-list '("◉" "○"))
    (org-bullets-mode 1)))

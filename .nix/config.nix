c470f80cba7e89423a9071e4114a85b0cd83725a
James ‘Twey’ Kay <twey@twey.co.uk>
2019-02-27 23:13:55 +0000

    working Agda environment

--- .nix/config.nix
+++ .nix/config.nix
@@ -1,3 +1,8 @@
 {
-  
+    packageOverrides = super: let self = super.pkgs; in
+    {
+        agda-env = self.haskell.packages.ghc863Binary.ghcWithPackages
+        (hpkgs: with hpkgs;
+         [ Agda ]);
+    }
 }

459255eed97c7682fe7166c77f6a3bd59643f9fc
James Kay <james@hadean.com>
2019-05-02 11:52:51 +0100

    nix: allowUnfree

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,8 +1,9 @@
 {
-    packageOverrides = super: let self = super.pkgs; in
-    {
-        agda-env = self.haskell.packages.ghc863Binary.ghcWithPackages
-        (hpkgs: with hpkgs;
-         [ Agda ]);
-    }
+  allowUnfree = true;
+  packageOverrides = super: let self = super.pkgs; in
+  {
+    agda-env = self.haskell.packages.ghc844.ghcWithPackages
+    (hpkgs: with hpkgs;
+     [ Agda ]);
+  };
 }

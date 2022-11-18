743f9aacfd39d32d6367a61ce8aac24b859ac0af
James ‘Twey’ Kay <twey@twey.co.uk>
2022-11-18 19:41:45 +0000

    purple theme

--- nixos/machines/thurisaz.nix
+++ nixos/machines/thurisaz.nix
@@ -16,6 +16,24 @@
 
   console.font = "${pkgs.terminus_font}/share/consolefonts/ter-u20n.psf.gz";
 
+  # purple prompt (mostly copied from the default)
+  programs.bash.promptInit = ''
+    # Provide a nice prompt if the terminal supports it.
+    if [ "$TERM" != "dumb" -o -n "$INSIDE_EMACS" ]; then
+      PROMPT_COLOR="1;31m"
+      let $UID && PROMPT_COLOR="1;35m"
+      if [ -n "$INSIDE_EMACS" -o "$TERM" == "eterm" -o "$TERM" == "eterm-color" ]; then
+        # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
+        PS1="\n\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
+      else
+        PS1="\n\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+      fi
+      if test "$TERM" = "xterm"; then
+        PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+      fi
+    fi
+  '';
+
   boot.loader.systemd-boot.enable = true;
   boot.initrd.luks.devices.root = {
     device = "/dev/disk/by-label/thurisaz";

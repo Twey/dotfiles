ae39695759b80c27fe2fcaa173877e124e5157a5
James ‘Twey’ Kay <twey@twey.co.uk>
2022-11-18 19:41:27 +0000

    luks instead of zfs

--- nixos/machines/thurisaz.nix
+++ nixos/machines/thurisaz.nix
@@ -16,6 +16,12 @@
 
   console.font = "${pkgs.terminus_font}/share/consolefonts/ter-u20n.psf.gz";
 
+  boot.loader.systemd-boot.enable = true;
+  boot.initrd.luks.devices.root = {
+    device = "/dev/disk/by-label/thurisaz";
+    preLVM = true;
+    allowDiscards = true;
+  };
   boot.extraModulePackages = [ pkgs.linuxPackages.openrazer ];
   boot.kernelModules = [ "razercore" "razermouse" "i2c-dev" "i2c-piix4" ];
 
@@ -25,5 +31,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.03"; # Did you read the comment?
+  system.stateVersion = "22.05"; # Did you read the comment?
 }

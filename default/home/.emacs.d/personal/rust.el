f19a5c29d8ae91b0bf6de00f978b52dac4216997
James ‘Twey’ Kay <twey@twey.co.uk>
2022-12-17 14:46:09 +0000

    overlay host-specific stow directory before stowing

--- default/home/.emacs.d/personal/rust.el
+++ default/home/.emacs.d/personal/rust.el
@@ -1,5 +1,8 @@
+(setq rust-format-on-save nil)
+
 (add-hook
  'rust-mode-hook
  (lambda ()
    (add-to-list 'write-file-functions #'delete-trailing-whitespace)
-   (local-set-key (kbd "<f12>") #'rust-test)))
+   (local-set-key (kbd "<f12>") #'rust-test)
+   (setq rust-format-on-save nil)))
--- default/home/.emacs.d/personal/text.el
+++ default/home/.emacs.d/personal/text.el
@@ -2,10 +2,10 @@
 (defface org-bullets
   '((t :family "DejaVu Sans Mono"))
   "Face used for highlighting bullets in org-bullets-mode.")
+
 (add-hook
- 'text-mode-hook
+ 'org-mode-hook
  (lambda ()
-   ; (variable-pitch-mode 1) ; I got over this tbh
    (org-indent-mode 1)
    (org-bullets-mode 1)))
 

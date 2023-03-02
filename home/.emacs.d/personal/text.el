73b680adf66f6e50c4703f5c55084f2654b70d7c
James ‘Twey’ Kay <twey@twey.co.uk>
2023-03-02 10:25:01 +0000

    fix emacs text rendering

--- home/.emacs.d/personal/text.el
+++ home/.emacs.d/personal/text.el
@@ -2,10 +2,16 @@
 (defface org-bullets
   '((t :family "DejaVu Sans Mono"))
   "Face used for highlighting bullets in org-bullets-mode.")
+;; (add-hook
+;;  'text-mode-hook
+;;  (lambda ()
+;;    ; (variable-pitch-mode 1) ; I got over this tbh
+;;    (org-indent-mode 1)
+;;    (org-bullets-mode 1)))
+
 (add-hook
- 'text-mode-hook
+ 'org-mode-hook
  (lambda ()
-   ; (variable-pitch-mode 1) ; I got over this tbh
    (org-indent-mode 1)
    (org-bullets-mode 1)))
 
